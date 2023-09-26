be3f9c90eacdee11a8e71b7b716ae877cb87393e
Jason <linux.solife@gmail.com>
2023-09-26 16:34:14 +0200

    Alignement du footer + éclaircissement thème light

--- config/footer.php
+++ config/footer.php
@@ -1,44 +1,39 @@
-<!-- INFORMATION: Le footer est à mettre en forme de box (côte à côte). -->
+<div class="footer-container">
+    <!-- A propos -->
+    <div class="footer-column">
+      <h2><i class="fas fa-address-card"></i> A propos</h2>
+      <p>
+        <b><?php echo $website_name; ?></b> est un projet fil-rouge fondé par <b>Précillia, Téo, Lucas, Yohann</b> et <b>Jason</b> durant leur stage au sein de la <a href="https://www.1formation-1job.com/nurserie/" target="_blank">Nurserie</a>.
+      </p>
+    </div>
 
-<!-- A propos -->
-<p class="footer-about">
-<h2><i class="fas fa-address-card"></i> A propos</h2>
-<br>
-<b>
-    <?php echo $website_name; ?>
-</b> est un projet fil-rouge fondé par <b>Précillia, Téo, Lucas, Yohann</b> et <b>Jason</b> durant leur stage au sein de
-la <a href="https://www.1formation-1job.com/nurserie/" target="_blank">Nurserie</a>.
-</p>
+    <!-- Conditions -->
+    <div class="footer-column">
+      <h2><i class="fas fa-list"></i> Liens utiles</h2>
+      <ul>
+        <!-- Ne pas toucher aux liens, on fera par pop-up ! -->
+        <li><a href="">Conditions d'utilisation</a></li>
+        <li><a href="">Conditions d'inscriptions</a></li>
+        <li><a href="">Nos délicieux cookies</a></li>
+      </ul>
+    </div>
 
-<!-- Conditions -->
-<p class="footer-conditions">
-<h2><i class="fas fa-list"></i> Liens utiles</h2>
-<br>
-<ul>
-    <!-- Ne pas toucher aux liens, on fera par pop-up ! -->
-    <li><a href="">Conditions d'utilisation</a></li>
-    <li><a href="">Conditions d'inscriptions</a></li>
-    <li><a href="">Nos délicieux cookies</a></li>
-</ul>
-</p>
+    <!-- Nos partenaires -->
+    <div class="footer-column">
+      <h2><i class="fas fa-handshake"></i> Nos partenaires</h2>
+      <ul>
+        <li><a href="#">Partenaire 1</a></li>
+        <li><a href="#">Partenaire 2</a></li>
+        <li><a href="#">Partenaire 3</a></li>
+        <li><a href="#">Partenaire 4</a></li>
+      </ul>
+    </div>
 
-<!-- Nos partenaires -->
-<p class="footer-partners">
-<h2><i class="fas fa-handshake"></i> Nos partenaires</h2>
-<br>
-<ul>
-    <li><a href="#">Partenaire 1</a></li>
-    <li><a href="#">Partenaire 2</a></li>
-    <li><a href="#">Partenaire 3</a></li>
-    <li><a href="#">Partenaire 4</a></li>
-</ul>
-</p>
-
-<!-- Suivez-nous -->
-<p class="footer-social">
-<h2><i class="fas fa-at"></i> Suivez-nous</h2>
-<br>
-<button id="btn-facebook"><i class="fab fa-facebook-f"></i> Facebook</button><br>
-<button id="btn-twitter"><i class="fab fa-twitter"></i> Twitter</button><br>
-<button id="btn-instagram"><i class="fab fa-instagram"></i> Instagram</button>
-</p>
+    <!-- Suivez-nous -->
+    <div class="footer-column">
+      <h2><i class="fas fa-at"></i> Suivez-nous</h2>
+      <button id="btn-facebook"><i class="fab fa-facebook-f"></i> Facebook</button><br>
+      <button id="btn-twitter"><i class="fab fa-twitter"></i> Twitter</button><br>
+      <button id="btn-instagram"><i class="fab fa-instagram"></i> Instagram</button>
+    </div>
+  </div>
